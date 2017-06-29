d39cde1fd11c0ae4b0ba44234eb41147d66741a5
prodhan29 <02nahid02@gmail.com>
2017-06-29 15:43:13 +0600

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import TreeList from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<TreeList />, document.getElementById('root'));
-registerServiceWorker();
