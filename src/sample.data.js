532befbb18825242df1c001d0ba5cffdaa45ba87
prodhan29 <02nahid02@gmail.com>
2017-06-30 14:10:03 +0600

    sample data updated

--- src/sample.data.js
+++ src/sample.data.js
@@ -1,12 +1,163 @@
 var treeData = {
+  groupDropOptionId: 39,
+  groupDropFieldId: 6,
+  parentId: 0,
   name: 'Bangladesh',
   exportValue: 'BD',
   showChildren: true,
   editMode: false,
   children: [
     {
-      name: 'khulna',
-      exportValue: 1,
+      groupDropOptionId: 40,
+      groupDropFieldId: 6,
+      parentId: 39,
+      name: 'dhaka',
+      exportValue: '1',
+      showChildren: true,
+      editMode: false,
+      children: [
+        {
+          groupDropOptionId: 41,
+          groupDropFieldId: 6,
+          parentId: 40,
+          name: 'gulshan',
+          exportValue: '101',
+          showChildren: false,
+          editMode: false,
+          children: []
+        },
+        {
+          groupDropOptionId: 42,
+          groupDropFieldId: 6,
+          parentId: 40,
+          name: 'mohakhali',
+          exportValue: '102',
+          showChildren: true,
+          editMode: false,
+          children: [
+            {
+              groupDropOptionId: 43,
+              groupDropFieldId: 6,
+              parentId: 42,
+              name: 'shaheenbagh',
+              exportValue: '20201',
+              showChildren: false,
+              editMode: false,
+              children: []
+            }
+          ]
+        },
+        {
+          groupDropOptionId: 44,
+          groupDropFieldId: 6,
+          parentId: 40,
+          name: 'banani',
+          exportValue: '103',
+          showChildren: false,
+          editMode: false,
+          children: []
+        },
+        {
+          groupDropOptionId: 45,
+          groupDropFieldId: 6,
+          parentId: 40,
+          name: 'dhanmondi',
+          exportValue: '204',
+          showChildren: true,
+          editMode: false,
+          children: [
+            {
+              groupDropOptionId: 46,
+              groupDropFieldId: 6,
+              parentId: 45,
+              name: 'jigatola',
+              exportValue: '20401',
+              showChildren: true,
+              editMode: false,
+              children: [
+                {
+                  groupDropOptionId: 47,
+                  groupDropFieldId: 6,
+                  parentId: 46,
+                  name: 'japan bangladesh hospital !!!!!!!!',
+                  exportValue: '2040101',
+                  showChildren: false,
+                  editMode: false,
+                  children: []
+                }
+              ]
+            },
+            {
+              groupDropOptionId: 48,
+              groupDropFieldId: 6,
+              parentId: 45,
+              name: 'staff quater',
+              exportValue: '12098',
+              showChildren: false,
+              editMode: false,
+              children: []
+            }
+          ]
+        },
+        {
+          groupDropOptionId: 49,
+          groupDropFieldId: 6,
+          parentId: 40,
+          name: 'motijheel',
+          exportValue: '205',
+          showChildren: false,
+          editMode: false,
+          children: []
+        }
+      ]
+    },
+    {
+      groupDropOptionId: 50,
+      groupDropFieldId: 6,
+      parentId: 39,
+      name: 'chittagong',
+      exportValue: '2',
+      showChildren: true,
+      editMode: false,
+      children: [
+        {
+          groupDropOptionId: 51,
+          groupDropFieldId: 6,
+          parentId: 50,
+          name: 'potenga',
+          exportValue: '201',
+          showChildren: false,
+          editMode: false,
+          children: []
+        },
+        {
+          groupDropOptionId: 52,
+          groupDropFieldId: 6,
+          parentId: 50,
+          name: 'bandarban',
+          exportValue: '202',
+          showChildren: false,
+          editMode: false,
+          children: []
+        },
+        {
+          groupDropOptionId: 53,
+          groupDropFieldId: 6,
+          parentId: 50,
+          name: 'cox\'s bazar',
+          exportValue: '203',
+          showChildren: false,
+          editMode: false,
+          children: []
+        }
+      ]
+    },
+    {
+      groupDropOptionId: 54,
+      groupDropFieldId: 6,
+      parentId: 39,
+      name: 'sylhet',
+      exportValue: '3',
       showChildren: false,
       editMode: false,
       children: []
