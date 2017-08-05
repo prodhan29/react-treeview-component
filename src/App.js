6dee9a4bc4230a2456439f0bc3a632240e5d64e7
prodhan29 <02nahid02@gmail.com>
2017-08-05 20:40:08 +0600

    removed unnecessary _this variable. used es6 syntax to avoid _this variable

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ export default class Treeview extends Component {
 
   constructor(props) {
     super(props);
-    let _this = this;
     this.state = {
       data: TreeData,
       editableNode: ''
@@ -91,7 +90,6 @@ export default class Treeview extends Component {
   }
 
   nodeEditForm = (value, parent, index) => {
-    let _this = this;
     return (
       <div className="node node_edit" onClick={(e) => { e.stopPropagation() }}>
         <form className="node_edit_form">
@@ -100,7 +98,7 @@ export default class Treeview extends Component {
               type="text"
               name='name'
               placeholder='Option'
-              onChange={function (e) { _this.handleEditChange(e, value) }}
+              onChange={(e)=> { this.handleEditChange(e, value) }}
             />
           </div>
           <div className="field code">
@@ -108,12 +106,12 @@ export default class Treeview extends Component {
               type="text"
               name='exportValue'
               placeholder='Value'
-              onChange={function (e) { _this.handleEditChange(e, value) }}
+              onChange={(e)=> { this.handleEditChange(e, value) }}
             />
           </div>
           <div className="field action_node">
-            <span className="fa fa-check" onClick={function (e) { _this.doneEdit(value) }}></span>
-            <span className="fa fa-close" onClick={function (e) { _this.closeForm(value, parent, index) }}></span>
+            <span className="fa fa-check" onClick={(e)=> { this.doneEdit(value) }}></span>
+            <span className="fa fa-close" onClick={(e)=> { this.closeForm(value, parent, index) }}></span>
           </div>
         </form>
       </div>
@@ -123,27 +121,26 @@ export default class Treeview extends Component {
   makeChildren = (node) => {
     if (typeof node === 'undefined' || node.length === 0) return null;
 
-    let _this = this;
     let children;
-    children = node.map(function (value, index) {
+    children = node.map((value, index)=> {
 
       let item = null;
 
       // A node has children and want to show her children
       if (value.children.length > 0 && value.showChildren) {
-        let babies = _this.makeChildren(value.children);
+        let babies = this.makeChildren(value.children);
         let normalMode = (
           <div className="node">
             <i className="fa fa-minus-square-o"></i>{value.name}
             <span className="actions">
-              <i className="fa fa-close" onClick={function (e) { e.stopPropagation(); _this.deleteNode(node, index) }}></i>
-              <i className="fa fa-pencil" onClick={function (e) { e.stopPropagation(); _this.makeEditable(value) }}></i>
+              <i className="fa fa-close" onClick={(e)=> { e.stopPropagation(); this.deleteNode(node, index) }}></i>
+              <i className="fa fa-pencil" onClick={(e)=> { e.stopPropagation(); this.makeEditable(value) }}></i>
             </span>
           </div>
         )
         item = (
-          <li key={index} onClick={(e) => { e.stopPropagation(); _this.toggleView(value) }}>
-            {(value.editMode) ? _this.nodeEditForm(value, node, index) : normalMode}
+          <li key={index} onClick={(e) => { e.stopPropagation(); this.toggleView(value) }}>
+            {(value.editMode) ? this.nodeEditForm(value, node, index) : normalMode}
             {babies}
           </li>
         )
@@ -152,7 +149,7 @@ export default class Treeview extends Component {
       // A node has children but don't want to showing her children
       else if (value.children.length > 0 && !value.showChildren) {
         item = (
-          <li key={index} onClick={function (e) { e.stopPropagation(); _this.toggleView(value) }}>
+          <li key={index} onClick={(e)=> { e.stopPropagation(); this.toggleView(value) }}>
             <div className="node"><i className="fa fa-plus-square-o"></i>{value.name}</div>
           </li>
         );
@@ -163,16 +160,16 @@ export default class Treeview extends Component {
         let normalMode = (
           <div className="node"><i className="fa fa-square-o"></i>{value.name}
             <span className="actions">
-              <i className="fa fa-plus" onClick={function (e) { e.stopPropagation(); _this.addChild(value) }}> </i>
-              <i className="fa fa-pencil" onClick={function (e) { e.stopPropagation(); _this.makeEditable(value) }}></i>
-              <i className="fa fa-close" onClick={function (e) { e.stopPropagation(); _this.deleteNode(node, index) }}></i>
+              <i className="fa fa-plus" onClick={(e)=> { e.stopPropagation(); this.addChild(value) }}> </i>
+              <i className="fa fa-pencil" onClick={(e)=> { e.stopPropagation(); this.makeEditable(value) }}></i>
+              <i className="fa fa-close" onClick={(e)=> { e.stopPropagation(); this.deleteNode(node, index) }}></i>
             </span>
           </div>
         );
 
         item = (
           <li key={index} onClick={(e) => e.stopPropagation()}>
-            {(value.editMode) ? _this.nodeEditForm(value, node, index) : normalMode}
+            {(value.editMode) ? this.nodeEditForm(value, node, index) : normalMode}
           </li>
         );
       }
@@ -183,24 +180,23 @@ export default class Treeview extends Component {
       <ul >
         {children}
         <li>
-          <div className="node add_node" onClick={function (e) { e.stopPropagation(); _this.addMember(node) }}>
+          <div className="node add_node" onClick={(e)=> { e.stopPropagation();this.addMember(node) }}>
             <i className="fa fa-square" ></i>
             <a >Add New</a>
           </div>
         </li>
       </ul>
-    )
+    );
   }
 
   getNodes = () => {
     if (typeof this.state.data.name === 'undefined') return null;
-    let _this = this;
     let children = this.makeChildren(this.state.data.children);
     let root = (
       <span className="root">{this.state.data.name}
         <span className="actions"> &nbsp;  &nbsp;
-                    <i className="fa fa-pencil" onClick={function (e) { e.stopPropagation(); _this.makeEditable(_this.state.data) }}> edit </i>
-          <i className="fa fa-plus" onClick={function (e) { e.stopPropagation(); _this.addChild(_this.state.data) }}> Add_child </i>
+                    <i className="fa fa-pencil" onClick={(e)=> { e.stopPropagation(); this.makeEditable(this.state.data) }}> edit </i>
+          <i className="fa fa-plus" onClick={(e)=> { e.stopPropagation(); this.addChild(this.state.data) }}> Add_child </i>
         </span>
       </span>
 
